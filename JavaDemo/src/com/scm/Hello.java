0ed2e31e2f3f08726821318738c0a503620c562d
vikas-41298 <vikasprajapati.4321@gmail.com>
2020-10-28 18:46:12 +0530

    add demo

---
+++ JavaDemo/src/com/scm/Hello.java
@@ -0,0 +1,13 @@
+package com.scm;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int i ,j;
+		i=10;
+		j=12;
+		
+		System.out.println("hello world"+" "+(i+j)+"added");
+	}
+
+}
